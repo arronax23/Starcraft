1e6a145c6c8033a7a8d89f320194b9b6a35332e8
Wiktor <wiktorzielak@gmail.com>
2019-07-07 11:18:02 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+function stronaGlowna(){
+    document.getElementsByTagName('main')[0].innerHTML = 
+    `        <h3>Strona główna</h3>
+    <p>
+            
+            StarCraft Remastered to gruntownie odświeżona wersja obowiązkowej pozycji z gatunku strategii science-fiction. Zagrajcie ponownie w pierwszą część gry i wielokrotnie nagradzany dodatek StarCraft: Brood War.
+            Odświeżyliśmy wygląd jednostek, budowli i scenerii, poprawiliśmy warstwę dźwiękową i poszerzyliśmy obsługiwane rozdzielczości. Ilustrowane przerywniki między misjami tchną nowe życie w zmagania i zwycięstwa takich bohaterów jak Artanis, Fenix, Tassadar, Raynor i Kerrigan.
+    </p>`;
+}
+
+function terran(){
+    document.getElementsByTagName('main')[0].innerHTML = 
+    `        <h3>Terran</h3>
+
+    <p>Terranie to zaradni koloniści, którzy wiele pokoleń temu wyruszyli z Ziemi i którymi rządzi teraz upadająca Konfederacja. Na peryferiach jej przestrzeni kosmicznej szeryf Jim Raynor musi połączyć siły z psioniczną zabójczynią, Sarą Kerrigan i grupą bojowników o wolność, aby przeżyć pierwsze spotkanie z protosami i zergami. Wkrótce wybucha wojna totalna, która obejmuje planety w całym sektorze, a Raynor uświadamia sobie, że w tej grze stawką jest coś więcej niż tylko przetrwanie jednostek.</p>`
+;
+}
+
+function protoss(){
+    document.getElementsByTagName('main')[0].innerHTML =
+    `        <h3>Protoss</h3>
+
+    <p>Starożytni protosi żyją w harmonii i jedności – łączy ich bowiem pradawna więź psioniczna zwana Khalą. Ci, którzy się jej wyrzekli na rzecz indywidualizmu, zostali dawno temu wygnani w chłodny mrok kosmosu. Protosom udało się stworzyć wspaniałe i wysoce rozwinięte społeczeństwo, lecz obowiązujący w nim sztywny system hierarchiczny może stać się przyczynkiem do buntu. Już wkrótce przekona się o tym młody egzekutor Artanis, któremu powierzono misję odnalezienia swojego poprzednika na jednym z zainfekowanych przez zergi światów.</p>`;
+}
+
+function zerg(){
+    document.getElementsByTagName('main')[0].innerHTML =
+    `        <h3>Zerg</h3>
+
+    <p>Po całym wieku przebywania na obrzeżach galaktyki, żarłoczne zergi dotarły na nieznane im dotąd planety w rzadko zaludnionym regionie kosmosu, zwanym sektorem Koprulu. Zaczęły infekować całe światy i pochłaniać wszystko na swojej drodze. Jednak za ich zwierzęcymi instynktami kryła się złowieszcza, inteligentna siła, która pchała te stworzenia do przekraczania własnych ograniczeń genetycznych i ciągłej ewolucji...</p>`;
+}
